44dbe7dbdc86473f06b30ac8a7773ce9bb84a34f
Brateq <brateq@gmail.com>
2016-07-18 15:59:49 +0200

    Add import with pagination

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -4,12 +4,15 @@ class Attraction < ActiveRecord::Base
 
     parse_page = Nokogiri::HTML(page)
     parse_page.css('.property_title').map do |a|
-      create(name: a.text, 
+      create(name: a.text,
              link: a.css('a').map { |link| link['href'] }.first)
     end
+    next_page = parse_page.css('.next').map { |a| a['href'] }.first
+    import_from_ta('https://pl.tripadvisor.com' + next_page) if next_page
   end
 
   def download_location
+    binding.pry
     parse_page = Nokogiri::HTML(open('https://tripadvisor.com' + link))
     cont = parse_page.css('.mapContainer').first
     return nil unless cont
