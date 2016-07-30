e100ea17fc0b109215cbb0ae3c5ad55b5652e7a7
brateq <brateq@gmail.com>
2016-07-30 12:04:24 +0200

    Clean up controllers

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,8 +1,6 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update, :destroy]
 
-  # GET /attractions
-  # GET /attractions.json
   def index
     @attractions = Attraction.where(visited: false)
     @hash = Gmaps4rails.build_markers(@attractions) do |attraction, marker|
@@ -19,57 +17,42 @@ class AttractionsController < ApplicationController
     end
   end
 
-  # GET /attractions/1
-  # GET /attractions/1.json
   def show
   end
 
-  # GET /attractions/new
   def new
     @attraction = Attraction.new
   end
 
-  # GET /attractions/1/edit
   def edit
   end
 
-  # POST /attractions
-  # POST /attractions.json
   def create
     @attraction = Attraction.new(attraction_params)
 
     respond_to do |format|
       if @attraction.save
         format.html { redirect_to @attraction, notice: 'Attraction was successfully created.' }
-        format.json { render :show, status: :created, location: @attraction }
       else
         format.html { render :new }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /attractions/1
-  # PATCH/PUT /attractions/1.json
   def update
     respond_to do |format|
       if @attraction.update(attraction_params)
         format.html { redirect_to @attraction, notice: 'Attraction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @attraction }
       else
         format.html { render :edit }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /attractions/1
-  # DELETE /attractions/1.json
   def destroy
     @attraction.destroy
     respond_to do |format|
       format.html { redirect_to attractions_url, notice: 'Attraction was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -77,7 +60,7 @@ class AttractionsController < ApplicationController
     url = params['ta_url']['url']
     Attraction.import_from_ta(url)
 
-    redirect_to :root, notice: 'Import in progress'
+    redirect_to :root, notice: 'Import done'
   end
 
   def reset
@@ -88,12 +71,10 @@ class AttractionsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_attraction
     @attraction = Attraction.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def attraction_params
     params.require(:attraction).permit(:name, :type, :link, :latitude, :longitude)
   end
