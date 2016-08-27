c97c3d62121ccdd978999c57b9b502e739be809c
brateq <brateq@gmail.com>
2016-08-27 15:58:19 +0200

    Add attraction statistics

--- app/decorators/attraction_decorator.rb
+++ app/decorators/attraction_decorator.rb
@@ -1,20 +1,27 @@
 class AttractionDecorator < Draper::Decorator
   delegate_all
 
-  def continents_stats
-    continents = Hash.new
-    object.uniq.pluck(:continent).each do |continent|
-      attractions = object.where(continent: continent).count
-      visited = object.where(continent: continent).visited.count
+  def stats(type, name)
+    statistics = Hash.new
+    next_region_type = next_type(type)
+    object.uniq.pluck(next_region_type).each do |current_region|
+      attractions = object.where(next_region_type => current_region).count
+      visited = object.where(next_region_type => current_region).visited.count
       percent = visited == 0 ? nil : h.number_to_percentage(visited.to_f / attractions.to_f * 100)
 
-      continent_stat =  { continent => { attractions: attractions,
-                                         visited: visited,
-                                         percent: percent } }
+      location_stat = { current_region => { attractions: attractions,
+                                          visited: visited,
+                                          percent: percent } }
 
-      continents.merge! continent_stat
+      statistics.merge! location_stat
     end
 
-    continents
+    statistics
+  end
+
+  def next_type(current_type)
+    types = %w(continent country region province municipality city island_group island)
+    index = types.index(current_type)
+    types[index + 1]
   end
 end
